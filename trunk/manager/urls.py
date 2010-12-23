a6bd81303b1f66b3591001109404516218fd6809
ciberjm <ciberjm@6504a3a9-5b6b-4057-975f-0ceee33eb5be>
2010-12-23 17:23:33 +0000

    [ciberjm] Subido urls.py corregido
    git-svn-id: file:///home/ddlara/Proyecto/svn/cusl5-90manager@7 6504a3a9-5b6b-4057-975f-0ceee33eb5be

---
+++ trunk/manager/urls.py
@@ -0,0 +1,40 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^manager/', include('manager.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+	(r'^/?$', 'manager.equipos.views.index'),
+	
+	
+	# Modulo de login
+	(r'^registrarse/', 'manager.equipos.views.registrar_usuario'),
+    (r'^cuentas/logout/$', 'django.contrib.auth.views.logout'),
+   	(r'^cuentas/login/$', 'django.contrib.auth.views.login'),
+    (r'^cuentas/perfil/$', 'manager.equipos.views.perfil_usuario'),
+
+	# Modulo de equipos
+   	(r'^equipos/(?P<equipo_id>\d+)/$', 'manager.equipos.views.ver_equipo'),
+   	
+   	# Modulo de ligas
+   	(r'^ligas/crear_liga/$', 'manager.equipos.views.crear_liga'),
+   	(r'^ligas/(?P<liga_id>\d+)/$', 'manager.equipos.views.ver_liga'),
+   	
+   	# Modulo de jugadores
+   	(r'^jugadores/(?P<jugador_id>\d+)/$', 'manager.equipos.views.ver_jugador'),
+   	
+   	# Modulo de jornadas
+   	(r'^jornadas/(?P<jornada_id>\d+)/$', 'manager.equipos.views.ver_jornada'),
+   	
+   	# Modulo de jornadas
+   	(r'^partidos/(?P<partido_id>\d+)/$', 'manager.equipos.views.ver_partido'),
+)
